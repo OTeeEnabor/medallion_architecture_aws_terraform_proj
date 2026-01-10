829b73b2e0e5d625bca14d93d543a068c9416d69
OTeeEnabor <tobienabor@gmail.com>
2026-01-10 23:13:42 +0200

    missed the test key for test glue job script.

--- modules/glue-jobs/main.tf
+++ modules/glue-jobs/main.tf
@@ -15,7 +15,7 @@ resource "aws_glue_job" "test_deployment_script" {
   command {
     name            = "glueetl"
     python_version  = "3"
-    script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}testDeployScript.py"
+    script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}/test/testDeployScript.py"
   }
 
   default_arguments = {
@@ -28,55 +28,55 @@ resource "aws_glue_job" "test_deployment_script" {
   tags = var.tags
 }
 
-resource "aws_glue_job" "bronze_to_silver" {
-  name              = "${var.project}-${var.environment}-bronze-to-silver"
-  role_arn          = var.glue_role_arn
-  glue_version      = var.glue_version
-  worker_type       = var.worker_type
-  number_of_workers = var.number_of_workers
+# resource "aws_glue_job" "bronze_to_silver" {
+#   name              = "${var.project}-${var.environment}-bronze-to-silver"
+#   role_arn          = var.glue_role_arn
+#   glue_version      = var.glue_version
+#   worker_type       = var.worker_type
+#   number_of_workers = var.number_of_workers
 
-  command {
-    name            = "glueetl"
-    python_version  = "3"
-    script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}bronze_to_silver.py"
-  }
+#   command {
+#     name            = "glueetl"
+#     python_version  = "3"
+#     script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}bronze_to_silver.py"
+#   }
 
-  default_arguments = {
-    "--job-language"   = "python"
-    "--enable-metrics" = "true"
-    "--BRONZE_PREFIX"  = var.bronze_prefix
-    "--SILVER_PREFIX"  = var.silver_prefix
-    "--PROJECT"        = var.project
-    "--ENV"            = var.environment
-  }
+#   default_arguments = {
+#     "--job-language"   = "python"
+#     "--enable-metrics" = "true"
+#     "--BRONZE_PREFIX"  = var.bronze_prefix
+#     "--SILVER_PREFIX"  = var.silver_prefix
+#     "--PROJECT"        = var.project
+#     "--ENV"            = var.environment
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-resource "aws_glue_job" "silver_to_gold" {
-  name              = "${var.project}-${var.environment}-silver-to-gold"
-  role_arn          = var.glue_role_arn
-  glue_version      = var.glue_version
-  worker_type       = var.worker_type
-  number_of_workers = var.number_of_workers
+# resource "aws_glue_job" "silver_to_gold" {
+#   name              = "${var.project}-${var.environment}-silver-to-gold"
+#   role_arn          = var.glue_role_arn
+#   glue_version      = var.glue_version
+#   worker_type       = var.worker_type
+#   number_of_workers = var.number_of_workers
 
-  command {
-    name            = "glueetl"
-    python_version  = "3"
-    script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}silver_to_gold.py"
-  }
+#   command {
+#     name            = "glueetl"
+#     python_version  = "3"
+#     script_location = "s3://${var.s3_data_lake}/${var.scripts_prefix}silver_to_gold.py"
+#   }
 
-  default_arguments = {
-    "--job-language"   = "python"
-    "--enable-metrics" = "true"
-    "--SILVER_PREFIX"  = var.silver_prefix
-    "--GOLD_PREFIX"    = var.gold_prefix
-    "--PROJECT"        = var.project
-    "--ENV"            = var.environment
-  }
+#   default_arguments = {
+#     "--job-language"   = "python"
+#     "--enable-metrics" = "true"
+#     "--SILVER_PREFIX"  = var.silver_prefix
+#     "--GOLD_PREFIX"    = var.gold_prefix
+#     "--PROJECT"        = var.project
+#     "--ENV"            = var.environment
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-output "bronze_to_silver_job_name" { value = aws_glue_job.bronze_to_silver.name }
-output "silver_to_gold_job_name" { value = aws_glue_job.silver_to_gold.name }
+# output "bronze_to_silver_job_name" { value = aws_glue_job.bronze_to_silver.name }
+# output "silver_to_gold_job_name" { value = aws_glue_job.silver_to_gold.name }
